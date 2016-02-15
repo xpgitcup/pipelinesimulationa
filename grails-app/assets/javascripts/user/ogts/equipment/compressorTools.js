fc1e145f2b0df7b0d98bf7d2ac8b5e0897a5a1b2
Win10Lxp <Win10Lxp@ChuyunWin10Lxp>
2016-02-15 17:12:37 +0800

    20160215--使用Tabs实现向导。

--- grails-app/assets/javascripts/user/ogts/equipment/compressorTools.js
+++ grails-app/assets/javascripts/user/ogts/equipment/compressorTools.js
@@ -8,25 +8,36 @@ $(function () {
     var appendTextDiv = $("#appendActionsText");
     //console.info(appendTextDiv);
     var appendText = appendTextDiv.text();
-    //console.info(appendText);
-    var appendArray = appendText.split(";");
+    console.info(appendText);
+    var appendArray = appendText.split(",");
     console.info(appendArray);
 
-    var ul = $("#action");
+    var ul = $("#actions");
     console.info(ul);
+    var li_href_str;
+    
+    var tabDiv = $('#newCompressorCurveTestTab');
+    console.info(tabDiv);
 
     $.each(appendArray, function (index, value) {
         //console.info(index);
-        //console.info(value);
+        console.info(value);
         var action = value.split(":");
         console.info(action);
         //var $nstr =$("<li href=\"javascript:" + action[1] + "()\">" + action[0] + "</li>"); 
-        var nstr = "<li> <a href=\"javascript:" + action[1] + "()\">" + action[0] + "</a></li>";
-        console.info(nstr);
-        ul.append(nstr);
+        //显示处理步骤中的各个按钮
+        li_href_str = "<li> <a href=\"javascript:" + action[1] + "()\">" + action[0] + "</a></li>";
+        //console.info(nstr);
+        ul.append(li_href_str);
+        
+        //显示各个Tab页面
+        tabDiv.tabs("add", {title: value, content: index + value});
     });
-    
-    checkGasName();
 });
 
+function queryCompressor() {}
+function queryTest() {}
+function choiceCompressor() {}
+function importTest() {}
+function compressorTools() {}
 
